b8c686a3510c90eb56484a01ed884e14c5be1a57
Hamza Aarab <haarab@c1r1p4.1337.ma>
2023-05-21 16:59:03 +0100

    hamza

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 18:22:52 by haarab            #+#    #+#             */
-/*   Updated: 2023/05/21 16:45:25 by haarab           ###   ########.fr       */
+/*   Updated: 2023/05/21 16:57:52 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,10 @@ int main(int ac, char **av)
 	if (ac != 1)
 		return (0);
 	// pthread_t t1, t2, t3, t4;
-	pthread_t th[4];
+	pthread_t th[16];
 	i = 0;
 	pthread_mutex_init(&mutex, NULL);
-	while (i < 4)
+	while (i < 16)
 	{
 		if (pthread_create(th + i, NULL, &routine, NULL) != 0)
 		{
@@ -52,6 +52,11 @@ int main(int ac, char **av)
 			return 1;
 		}
 		printf("thread %d has started\n", i);
+		i++;
+	}
+	i = 0;
+	while (i < 16)
+	{
 		if (pthread_join(th[i], NULL) != 0)
 		{
 			return (2);
@@ -63,6 +68,20 @@ int main(int ac, char **av)
 	printf ("str === %d\n", mails);
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -124,6 +143,37 @@ int main(int ac, char **av)
 // 	pthread_join(t2, NULL);
 // 	pthread_join(t3, NULL);
 // 	pthread_join(t4, NULL);
+// 	pthread_mutex_destroy(&mutex);
+// 	printf ("str === %d\n", mails);
+// 	return (0);
+// }
+
+// int main(int ac, char **av)
+// {
+// 	int i;
+
+// 	(void) av;
+// 	if (ac != 1)
+// 		return (0);
+// 	// pthread_t t1, t2, t3, t4;
+// 	pthread_t th[4];
+// 	i = 0;
+// 	pthread_mutex_init(&mutex, NULL);
+// 	while (i < 4)
+// 	{
+// 		if (pthread_create(th + i, NULL, &routine, NULL) != 0)
+// 		{
+// 			printf ("failed to create thread");
+// 			return 1;
+// 		}
+// 		printf("thread %d has started\n", i);
+// 		if (pthread_join(th[i], NULL) != 0)
+// 		{
+// 			return (2);
+// 		}
+// 		printf("thread %d has finished\n", i);
+// 		i++;
+// 	}
 // 	pthread_mutex_destroy(&mutex);
 // 	printf ("str === %d\n", mails);
 // 	return (0);
