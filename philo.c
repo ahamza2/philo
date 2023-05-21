5defa4c75249a551c05362448e7431e2f20feaff
Hamza Aarab <haarab@c1r1p4.1337.ma>
2023-05-21 15:14:13 +0100

    hamza

--- philo.c
+++ philo.c
@@ -6,23 +6,26 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 18:22:52 by haarab            #+#    #+#             */
-/*   Updated: 2023/05/21 13:04:24 by haarab           ###   ########.fr       */
+/*   Updated: 2023/05/21 15:13:17 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 int mails = 0;
+pthread_mutex_t mutex;
 
 void	*routine()
 {
 	int i;
 	
 	i = 0;
-	while (i < 4)
+	while (i < 10000000)
 	{
+		pthread_mutex_lock(&mutex);
 		mails++;
-		printf ("ptr === %d\n", mails);
+		pthread_mutex_unlock(&mutex);
+		// printf ("ptr === %d\n", mails);
 		i++;
 	}
 	return (NULL);
@@ -34,10 +37,12 @@ int main(int ac, char **av)
 	if (ac != 1)
 		return (0);
 	pthread_t t1, t2;
+	pthread_mutex_init(&mutex, NULL);
 	pthread_create(&t1, NULL, &routine, NULL);
 	pthread_create(&t2, NULL, &routine, NULL);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	pthread_mutex_destroy(&mutex);
 	printf ("str === %d\n", mails);
 	return (0);
 }
