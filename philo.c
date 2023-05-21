6a3d5b5c962fbf2c1430e2a79a61931b7e25a0a3
Hamza Aarab <haarab@c1r1p4.1337.ma>
2023-05-21 14:14:50 +0100

    push

--- philo.c
+++ philo.c
@@ -6,17 +6,25 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 18:22:52 by haarab            #+#    #+#             */
-/*   Updated: 2023/05/20 20:25:55 by haarab           ###   ########.fr       */
+/*   Updated: 2023/05/21 13:04:24 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int mails = 0;
+
 void	*routine()
 {
-	printf("hamza\n");
-	sleep(2);
-	printf("aarab\n");
+	int i;
+	
+	i = 0;
+	while (i < 4)
+	{
+		mails++;
+		printf ("ptr === %d\n", mails);
+		i++;
+	}
 	return (NULL);
 }
 
@@ -24,11 +32,12 @@ int main(int ac, char **av)
 {
 	(void) av;
 	if (ac != 1)
-		return (2);
+		return (0);
 	pthread_t t1, t2;
 	pthread_create(&t1, NULL, &routine, NULL);
 	pthread_create(&t2, NULL, &routine, NULL);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	printf ("str === %d\n", mails);
 	return (0);
 }
